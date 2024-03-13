8e7c00646b5144ef9ab83280ed0543ea7b15db5b
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-13 11:43:50 +0000

    quick fixed

--- src/signup.py
+++ src/signup.py
@@ -1,11 +1,7 @@
-first_name = ""
-last_name = ""
-user_name = ""
-
 users = {
-    "firs_name": "",
-    "last_name":"",
-    "user_name": ""
+    "firs_name": [],
+    "last_name":[],
+    "user_name": []
 }
 
 class signup:
@@ -17,7 +13,7 @@ class signup:
 
         return (first_name, last_name, user_name)
 
-    def add_user(self):
+    def add_user(self, first_name, last_name, user_name):
         users["first_name"].append(first_name)
         users["last_name"].append(last_name)
         users["user_name"].append(user_name)
