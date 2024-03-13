ec8ea232c4ed733117d447e7ce09aff5cd8281ec
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-13 11:47:17 +0000

    sign up message displayed

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+import signup
+
 def main():
-    pass
+    welcome()
+    signup_message()
 
 def welcome():
     print("===PyExpenseTracker===")
+
+def signup_message():
+    print("please sign up to continue!")
+
+main()
