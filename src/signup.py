d08d5934838d8baeeb53276ca231eefad5d44970
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-13 12:12:15 +0000

    implemented adding users to json file

--- src/signup.py
+++ src/signup.py
@@ -1,25 +1,31 @@
-users = {
-    "first_name": [],
-    "last_name":[],
-    "user_name": []
-}
+import json
 
 class Signup:
     
+    def __init__(self):
+        self.users = self.load_users()
+
+    def load_users(self):
+        try:
+            with open("users.json", "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return {"first_name": [], "last_name": [], "user_name": []}
+
     def create_user(self):
-        first_name = input("enter your first name: ")
+        first_name = input("Enter your first name: ")
         last_name = input("Enter your last name: ")
-        user_name = input("enter you user_name: ")
+        user_name = input("Enter your user name: ")
 
         return (first_name, last_name, user_name)
 
     def add_user(self, first_name, last_name, user_name):
-        users["first_name"].append(first_name)
-        users["last_name"].append(last_name)
-        users["user_name"].append(user_name)
+        self.users["first_name"].append(first_name)
+        self.users["last_name"].append(last_name)
+        self.users["user_name"].append(user_name)
 
+        with open("users.json", "w") as file:
+            json.dump(self.users, file)
 
-        return (users)
-    def display_users(self):
-        print(users)
+        return self.users
 
