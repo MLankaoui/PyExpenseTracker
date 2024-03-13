1278be1a695528b2027fc54800ba4f9212c927c6
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-13 11:41:37 +0000

    first attempts of implementing user signup

---
+++ src/signup.py
@@ -0,0 +1,26 @@
+first_name = ""
+last_name = ""
+user_name = ""
+
+users = {
+    "firs_name": "",
+    "last_name":"",
+    "user_name": ""
+}
+
+class signup:
+    
+    def create_user(self):
+        first_name = input("enter your first name: ")
+        last_name = input("Enter your last name: ")
+        user_name = input("enter you user_name: ")
+
+        return (first_name, last_name, user_name)
+
+    def add_user(self):
+        users["first_name"].append(first_name)
+        users["last_name"].append(last_name)
+        users["user_name"].append(user_name)
+
+
+        return (users)
